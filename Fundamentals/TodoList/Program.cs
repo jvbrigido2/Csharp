c994b2e703d894f5d9df0d642d96fc4d28582785
Joao Brigido <jv_brigido@hotmail.com>
2024-07-29 10:31:53 -0300

    update: Implement the switch for select options of todolist

--- Fundamentals/TodoList/Program.cs
+++ Fundamentals/TodoList/Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("TodoList");
+﻿// Todolist assignment
+
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[S]ee all todos");
+Console.WriteLine("[A]dd a todo");
+Console.WriteLine("[R]emove a todo");
+Console.WriteLine("[E]xit");
+
+var userChoice = Console.ReadLine();
+
+switch (userChoice)
+{
+    case "S":
+    case "s":
+        Console.WriteLine("See a TODO");
+        break;
+    case "A":
+    case "a":
+        Console.WriteLine("Add a TODO");
+        break;
+    case "R":
+    case "r":
+        Console.WriteLine("Remove a TODO");
+        break;
+    case "E":
+    case "e":
+        Console.WriteLine("Exit");
+        break;
+    default:
+        Console.WriteLine("Invalid Option");
+        break;
+}
+
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+
+
